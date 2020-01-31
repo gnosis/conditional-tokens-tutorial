a88552b98370b5b46c204b17fd3d50a808392d05
Germán Martínez <germaga92@gmail.com>
2020-01-31 17:28:47 +0100

    Add markets config files

---
+++ markets.config.js
@@ -0,0 +1,16 @@
+module.exports = [
+  {
+    questionId: "0x4b22fe478b95fdaa835ddddf631ab29f12900b62061e0c5fd8564ddb7b684333",
+    title: "Will the summer 2020 in Germany break again weather records? ",
+    outcomes: [
+      {
+        title: "Yes",
+        short: "Yes"
+      },
+      {
+        title: "No",
+        short: "No"
+      }
+    ]
+  }
+];
